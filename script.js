ebe1e1928f927171ff9017c8795900e4415332e7
ani1990ani <anahit.darbinian@gmail.com>
2024-01-13 13:35:49 +0400

    added 4 console logs

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello world 1");
+console.log("Hello world 2");
+console.log("Hello world 3");
+console.log("Hello world 4");
+//create new branch - git checkout -b branchName
+//git checkout branchName
+//git pull (remode ic local)
+//git add . bolory
+// git commit -m "added 4 console log"
+// git push (local ic tanuma remode)
